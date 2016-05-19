ac64e12d2967b338eebabf1a8575d209b67eda21
Alexey Egorychev <aegorychev@bistrodengi.ru>
2016-05-19 16:20:43 +0400

    Feature: disable and remove users

--- compare_users.py
+++ compare_users.py
@@ -3,7 +3,7 @@
 '''
 The script will compare the users from LDAP group list with the members of local Zimbra users.
 If the user is not in LDAP and present in the Zimbra, it will remove it from Zimbra.
-Tested on Zimbra FOSS 8
+Tested on Zimbra FOSS 8.
 '''
 
 # Config section
@@ -15,12 +15,14 @@ ldappassword = "P@ssw0rd!"
 ldapDefaultScope = "OU=Employees,OU=myOrg,DC=domain,DC=local"
 ldapSearchGroup = "CN=g_zimbra,OU=Groups,OU=myOrg,DC=domain,DC=local"
 ldapFields = ['sAMAccountName','sn','title','givenName','displayName','company']
+reExcludeUsers = "^spam.*|^ham.*|^galsync.*|^admin$|^virus-quarantine.*"
+emailExpireDay = 365
 pathtozmprov="/opt/zimbra/bin/zmprov"
 logfile='/var/log/zimbra-compare.log'
 pidfile='/var/run/zimbra-compare.pid'
 # end Config section
 
-import ldap, os, string, random, time, sys
+import ldap, os, string, random, time, sys, datetime, subprocess, re
 
 def getLdapSearchResult(ldapconnect,scope,fields):
     res = ldapconnect.search_s(scope,ldap.SCOPE_SUBTREE,"(&(objectClass=user)(memberOf="+ldapSearchGroup+"))",fields)
@@ -39,15 +41,17 @@ def filterADUsers(data,userList):
             filteredUsers.append(vals)
     return filteredUsers
 
-
-def getZimbraUsers():
-    zimbraUsers = []
-    users = os.popen(pathtozmprov + " -l gaa").read().split("\n")
-    for user in users:
-        user = user[:user.find('@')]
-        if user != '' and user.find('spam.') == -1 and user.find('ham.') == -1 and user.find('virus-quarantine.') == -1 and user.find('galsync.') == -1 and user != 'admin':
-            zimbraUsers.append(user)
-    return zimbraUsers
+def getDetailedZimbraUsers(fFilter=['zimbraLastLogonTimestamp','zimbraAccountStatus']):
+    userFieldsByKeys = {}
+    users = subprocess.Popen([pathtozmprov,"-l","gaa","-v","-e"],stdout=subprocess.PIPE)
+    for user in users.stdout.read().split("# name "):
+        if user != "":
+            userFields = user.split("\n")
+            userFieldsByKeys[userFields[0][:userFields[0].find('@')]] = {}
+            for uf in userFields:
+                 if uf.split(": ")[0] in fFilter:
+                     userFieldsByKeys[userFields[0][:userFields[0].find('@')]][uf.split(": ")[0]] = uf.split(": ")[1]
+    return userFieldsByKeys
 
 # get differents from to lists
 def diffLists (list1,list2):
@@ -77,21 +81,32 @@ l.simple_bind_s(ldapbinduser,ldappassword)
 result = getLdapSearchResult(l,ldapDefaultScope,ldapFields)
 ADUsers = getADUsers(result)
 
-zimbraUsers = getZimbraUsers()
+zimbraUsers = getDetailedZimbraUsers()
 
 
 needToAdd = []
+needToClose = []
 needToDel = []
-for i in diffLists(ADUsers,zimbraUsers):
+for i in diffLists(ADUsers,zimbraUsers.keys()):
     action = i[:1]
     user = i[1:]
     if action == '+':
         needToAdd.append(user)
     elif action == "-":
-        needToDel.append(user)
+        if not re.search(reExcludeUsers,user) and zimbraUsers[user]['zimbraAccountStatus'] != 'closed':
+            needToClose.append(user)
     else:
         print "Error: first symbol username in diff result must be '+' or '-'"
 
+presentTime = datetime.datetime.now()
+for i in zimbraUsers:
+        if "zimbraLastLogonTimestamp" not in zimbraUsers[i]:
+            zimbraUsers[i]["zimbraLastLogonTimestamp"] = "19700101000000Z"
+        userLastLogon = datetime.datetime.strptime(zimbraUsers[i]["zimbraLastLogonTimestamp"][:-1], "%Y%m%d%H%M%S")
+        if presentTime > userLastLogon + datetime.timedelta(days=emailExpireDay) and zimbraUsers[i]["zimbraAccountStatus"] == "closed":
+            needToDel.append(i)
+
+# Add users
 for user in filterADUsers(result,needToAdd):
     strFill = " "
     for f in ldapFields:
@@ -103,6 +118,12 @@ for user in filterADUsers(result,needToAdd):
     os.system("su - zimbra -c \""+command+"\"")
     log.write(time.strftime("%d.%m.%Y %H:%M:%S")+" Create user: "+user['sAMAccountName'][0].lower()+defaultDomain+"\n")
 
+# Disable users
+for user in needToClose:
+    os.system("su - zimbra -c \"zmprov ma "+user+defaultDomain+" zimbraAccountStatus closed\"")
+    log.write(time.strftime("%d.%m.%Y %H:%M:%S")+" Disable user: "+user+defaultDomain+"\n")
+
+# Delete users
 for user in needToDel:
     os.system("su - zimbra -c \"zmprov da "+user+defaultDomain+"\"")
     log.write(time.strftime("%d.%m.%Y %H:%M:%S")+" Delete user: "+user+defaultDomain+"\n")
